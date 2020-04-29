a7e79e8dcabbe739e9b2df5f1605c7fba7fa0bb7
Kossie Coder <kossie.coder@gmail.com>
2020-04-29 22:09:33 +1000

    02

---
+++ src/pages/login/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="mt-4">
+        <b-form @submit.prevent="onSubmit">
+            <b-form-group label="Email address:">
+                <b-form-input
+                    v-model="form.email"
+                    type="email"
+                    required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="Password:">
+                <b-form-input
+                    type="password"
+                    v-model="form.password"
+                    required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Login</b-button>
+        </b-form>
+    </div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				form: {
+					email: '',
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            onSubmit() {
+
+            }
+        }
+    }
+</script>
---
+++ src/pages/signup/index.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="mt-4">
+        <b-form @submit.prevent="onSubmit">
+            <b-form-group label="Name:">
+                <b-form-input
+                    v-model="form.name"
+                    type="text"
+                    required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="Email address:">
+                <b-form-input
+                    v-model="form.email"
+                    type="email"
+                    required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="Password:">
+                <b-form-input
+                    type="password"
+                    v-model="form.password"
+                    required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="Confirm Password:">
+                <b-form-input
+                    type="password"
+                    v-model="form.passwordConfirm"
+                    required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Sign Up</b-button>
+        </b-form>
+    </div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				form: {
+					email: '',
+					password: '',
+					passwordConfirm: '',
+                    name: ''
+				}
+			}
+		},
+		methods: {
+			onSubmit() {
+
+			}
+		}
+	}
+</script>
